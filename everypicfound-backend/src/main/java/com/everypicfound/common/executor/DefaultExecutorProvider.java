32100534cafb086318f378efe5ae063173127196
mxl <mxlscut153@163.com>
2026-05-28 21:29:21 +0800

    Implemented ThreadPool related classes, including ThreadPool Thread Manager / Pool Provider

--- everypicfound-backend/src/main/java/com/everypicfound/common/executor/DefaultExecutorProvider.java
+++ everypicfound-backend/src/main/java/com/everypicfound/common/executor/DefaultExecutorProvider.java
@@ -1,12 +1,25 @@
 package com.everypicfound.common.executor;
 
 import java.util.concurrent.Executor;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class DefaultExecutorProvider implements ExecutorProvider {
+
+    private final ThreadPoolManager threadPoolManager;
 
 
     // 根据业务类型返回 Executor；MVP 可统一返回公共线程池。
     @Override
     public Executor getExecutor(ExecutorBizType bizType) {
-        throw new UnsupportedOperationException("TODO");
+        if (bizType == null) {
+            return threadPoolManager.getCommonExecutor();
+        }
+
+        return threadPoolManager.getExecutorByType(bizType);
     }
 }
